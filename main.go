1c31a580e00abd7aa7f4a2e0ba0d09d8868d24b3
Brett Hartman <brett.hartman@mac.com>
2019-03-14 20:51:50 -0700

    Create first simple web server

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":3000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
