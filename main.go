b3fd729facb5ef94022a4126fad91ceded1de64e
Brett Hartman <brett.hartman@mac.com>
2019-03-14 22:15:34 -0700

    Include page not found (404) route

--- main.go
+++ main.go
@@ -17,5 +17,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<p>To get in touch, please send an e-mail to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.</p>")
 	} else if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h3>The page you were looking for could not be found.</h3>")
 	}
 }
